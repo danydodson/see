c0a381626a228bee790a4fa95a4b03c0083479d7
Dany Dodson <danydodson@gmail.com>
2021-01-07 00:52:07 -0600

    updates

--- web-client2/components/layout/Page.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import styles from './Page.module.css';
-
-export default function Page({ children }) {
-  return <div className={styles.page}>{children}</div>;
-}
--- web-client2/components/layout/PageHeader.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import styles from './PageHeader.module.css';
-
-export default function PageHeader({ children }) {
-  return <div className={styles.header}>{children}</div>;
-}
